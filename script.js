cd44fddd9be8066e9c52c79cae27bb82eb714389
Dexter Sage <dcsagework24@gmail.com>
2020-09-10 19:22:24 -0700

    updated variables

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  
+
+  var numberChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var upperChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowerChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var specialChars = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "`", "{", "|", "}", "~"];
+
   prompt ("Choose length of password between 8 and 120 characters.")
   confirm ("Would you like lowercase letters?'")
   confirm ("Would you like uppercase letters?'")
@@ -17,7 +22,9 @@ function writePassword() {
 
 }
 
-var number = [0,1,2,3,4,5,6,7,8,9]
+
+
+
 
 
 
